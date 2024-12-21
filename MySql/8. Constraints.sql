5af700ebfb7914884a5f5fdc8b70f33007aae66a
tbebekis <teo.bebekis@gmail.com>
2024-12-21 22:07:43 +0200

    initial

---
+++ MySql/8. Constraints.sql
@@ -0,0 +1,30 @@
+select distinct
+    Constraints.constraint_schema                     as SchemaName,  
+    Constraints.constraint_name                       as ConstraintName,
+    Constraints.constraint_type                       as ConstraintType,
+    KeyColumns.table_name                             as TableName,
+    KeyColumns.column_name                            as FieldName,    
+    coalesce(KeyColumns.REFERENCED_TABLE_NAME, '')    as ForeignTable,
+    coalesce(KeyColumns.REFERENCED_COLUMN_NAME, '')   as ForeignField,  
+    coalesce(KeyColumns.REFERENCED_TABLE_SCHEMA, '')  as ForeignSchema,
+    coalesce(Refs.update_rule, '')				      as UpdateRule,
+    coalesce(Refs.delete_rule, '')				      as DeleteRule,
+    KeyColumns.ordinal_position                       as FieldPosition   
+from 
+    INFORMATION_SCHEMA.TABLE_CONSTRAINTS Constraints
+        inner join INFORMATION_SCHEMA.KEY_COLUMN_USAGE KeyColumns            
+            on  (Constraints.constraint_catalog = KeyColumns.constraint_catalog or Constraints.constraint_catalog is null) 
+            and Constraints.constraint_schema = KeyColumns.constraint_schema 
+            and Constraints.constraint_name = KeyColumns.constraint_name 
+            and Constraints.table_name = KeyColumns.table_name
+        left join INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS Refs
+            on (Constraints.constraint_catalog = Refs.constraint_catalog or Constraints.constraint_catalog is null) 
+            and Constraints.constraint_schema = Refs.constraint_schema 
+            and Constraints.constraint_name = Refs.constraint_name 
+            and Constraints.table_name = Refs.table_name
+where
+    Constraints.constraint_schema not in ('sys', 'mysql', 'information_schema', 'performance_schema') 
+order by
+    KeyColumns.table_name, 
+    Constraints.constraint_name, 
+    KeyColumns.ordinal_position
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# Db-Schema-Info
-SELECT statements to get database schema information for various RDMS
+# Database Schema information for various RDBMS
+
+This repository contains a number of SQL SELECT statements to get Database Schema information for the following RDBMS
+
+- FirebirdSql
+- MsSql
+- MySql
+- PostgreSql
+- Sqlite
+- Oracle
+
+## Returned Field Lists of the SELECT statements
+The SELECT statements return the following Field Lists.
+
+### Tables
+- SchemaName
+- TableName
+### Views
+- SchemaName
+- TableName
+- Definition
+### Table and View Fields
+- SchemaName
+- TableName
+- FieldName
+- DataType 
+- DataSubType 
+- IsNullable
+- SizeInChars
+- SizeInBytes
+- DecimalPrecision
+- DecimalScale
+- DefaultValue
+- Expression
+- OrdinalPosition
+### Indexes
+- SchemaName
+- TableName
+- IndexName
+- FieldName 
+- FieldPosition
+- IsUnique     
+- IndexType 
+### Triggers
+- SchemaName
+- TableName
+- TriggerName
+- TriggerType
+- Definition
+### Procedures
+- SchemaName
+- ProcedureName
+- ProcedureType
+- Definition
+### Constraints
+- SchemaName
+- ConstraintName
+- ConstraintType
+- TableName
+- FieldName
+- ForeignTable
+- ForeignField
+- UpdateRule
+- DeleteRule
+- FieldPosition
+### Sequences
+- SchemaName
+- SequenceName
+- CurrentValue
+- InitialValue
+- IncrementBy	
+ 
+
+
